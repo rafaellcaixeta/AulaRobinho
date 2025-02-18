daa1a502f802586ae32b404d18e472ff80b41364
rafaellcaixeta <rafaellucianocaixeta@hotmail.com>
2025-02-18 19:52:26 -0300

    Add files via upload

---
+++ 18022025.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, C, R, S, D;
+
+            Console.WriteLine("Insira um numero inteiro e positivo:");
+            A = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira um numero inteiro e positivo:");
+            B = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira um numero inteiro e positivo:");
+            C = int.Parse(Console.ReadLine());
+
+            R = (A + B) * (A + B);
+            S = (B + C) * (B + C);
+            D = (R + S) / 2;
+
+            Console.WriteLine("Valor de D é: " + D);
+        }
+    }
+}
