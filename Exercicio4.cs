b87e455bc5abd206c586c130712292d58da5a3f6
rafaellcaixeta <rafaellucianocaixeta@hotmail.com>
2025-02-18 20:12:54 -0300

    Add files via upload

---
+++ Exercicio4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+
+            Console.WriteLine("Escreva um número inteiro:");
+            N = int.Parse(Console.ReadLine());
+
+            if (N % 2 == 0)
+                {
+                Console.WriteLine("O número inserido é par");
+            }
+            else
+            {
+                Console.WriteLine("O número inserido é impar");
+
+            }
+        }
+    }
+}
